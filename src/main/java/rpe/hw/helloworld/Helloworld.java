54484a4a250a3cdbc48de1d73fefaccc0936e608
Pavel Ryzhikov <pavelryzhikov@gmail.com>
2019-08-23 00:15:16 +0300

    init

---
+++ src/main/java/rpe/hw/helloworld/Helloworld.java
@@ -0,0 +1,12 @@
+package rpe.hw.helloworld;
+
+public class Helloworld {
+
+    public Helloworld () {
+        System.out.print("hi me");
+    }
+
+    public static void main(String[] args) {
+        System.out.print("hi rpe");
+    }
+}
